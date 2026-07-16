7968255ef9b192a021fafcc11157142e39561ab9
Vinayak Varshney <vinayak.varshney.dev@gmail.com>
2026-07-17 01:20:30 +0530

    added missing whatsapp endpoint to server.py

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ import uvicorn
 
 from backend.pulse_models.response_model import AgentResponse
 from backend.pulse_controllers.core_chat_controllers.chat_controller import process_chat
+from backend.pulse_controllers.whatsapp_chat_controllers.send_wa_text import openwa_webhook
 
 app = FastAPI(title="PulseAI Server", description="API server for PulseAI backend")
 
@@ -14,6 +15,11 @@ app.add_api_route("/api/v1/chat",
                   summary="Process chat requests"
                   )
 
+app.add_api_route("/api/v1/whatsapp/webhook",
+                  openwa_webhook,
+                  methods=["POST"],
+                  summary="Webhook for receiving WhatsApp messages"
+                  )
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
